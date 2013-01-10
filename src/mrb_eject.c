d249d196bded93cfe331cb0230d225dba56666f8
mattn <mattn.jp@gmail.com>
2013-01-11 02:15:23 +0900

    toggle/open/close.

--- src/mrb_eject.c
+++ src/mrb_eject.c
@@ -16,17 +16,27 @@
 mrb_value
 mrb_eject(mrb_state *mrb, mrb_value klass)
 {
+  mrb_value arg = mrb_nil_value();
+
+  mrb_get_args(mrb, "|o", &arg);
+
 #ifdef _WIN32
   mciSendString("set cdaudio door open", NULL, 0, NULL);
 #else
   int fd = open("/dev/cdrom", O_RDONLY | O_NONBLOCK);
-  if (fd != -1) {
-  }
   if (fd == -1) {
     perror("open");
-  } else if (ioctl(fd, CDROMEJECT, 0) < 0) {
-    perror("ioctl");
   } else {
+    int e = CDROMEJECT;
+    if (mrb_nil_p(arg)) {
+      if (ioctl(fd, CDROM_DRIVE_STATUS, 0) == CDS_TRAY_OPEN)
+        e = CDROMCLOSETRAY;
+    } else if (mrb_type(arg) == MRB_TT_TRUE)
+      e = CDROMEJECT;
+    else
+      e = CDROMCLOSETRAY;
+    if (ioctl(fd, e, 0) < 0)
+      perror("ioctl");
     close(fd);
   }
 #endif
